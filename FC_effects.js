8caffe36fc6102324d09006d0fe420599c90bba2
Amely Kling <mail@dwi.sk>
2017-04-17 22:05:50 +0200

    Pong Updates

--- FC_effects.js
+++ FC_effects.js
@@ -189,6 +189,72 @@ module.exports = [
 		]
 	},
 	{
+		name: "TischtennisStart",
+		module: "./effects/Canvas.js",
+		draw: [
+			{
+	      type: "map",
+	      position: [0, 0],
+	      map: [
+	        [0, 0, 0, 0, 0],
+	        [0, 0, 1, 0, 0]
+	      ],
+	      color: [255, 0, 0]
+	    },
+			{
+	      type: "map",
+	      position: [0, 0],
+	      map: [
+	        [1, 1, 1, 1, 1],
+	        [1, 1, 0, 1, 1]
+	      ],
+	      color: [0, 0, 0]
+	    }
+
+		],
+		outputs: [
+			{
+				name: "canvas",
+				targets: [
+					"buttons.strips"
+				]
+			}
+		]
+	},
+	{
+		name: "MazeStart",
+		module: "./effects/Canvas.js",
+		draw: [
+			{
+	      type: "map",
+	      position: [0, 0],
+	      map: [
+	        [1, 0, 0, 0, 0],
+	        [0, 0, 0, 0, 1]
+	      ],
+	      color: [255, 0, 0]
+	    },
+			{
+	      type: "map",
+	      position: [0, 0],
+	      map: [
+	        [0, 1, 1, 1, 1],
+	        [1, 1, 1, 1, 0]
+	      ],
+	      color: [0, 0, 0]
+	    }
+
+		],
+		outputs: [
+			{
+				name: "canvas",
+				targets: [
+					"buttons.strips"
+				]
+			}
+		]
+	},
+	{
 		name: "TitleDwisk",
 		module: "./effects/Canvas.js",
 		draw: [
